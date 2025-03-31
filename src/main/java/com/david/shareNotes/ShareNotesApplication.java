a13916f39a3dc0b24e1b1b8331ef8cc8fad9427f
davidtongeo <david1108n@gmail.com>
2025-03-31 09:15:11 -0500

    first commit

---
+++ src/main/java/com/david/shareNotes/ShareNotesApplication.java
@@ -0,0 +1,13 @@
+package com.david.shareNotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShareNotesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShareNotesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/david/shareNotes/ShareNotesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.david.shareNotes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShareNotesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
